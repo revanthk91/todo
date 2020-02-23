93e3db2a599daf1b657b539aaea5051141511546
Revanth Kumar <gamingkid45@gmail.com>
2020-02-23 09:17:50 +0530

    Updates

---
+++ precache-manifest.e08924945803b18d2b53ab61fa03b7ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe6983266c15aef402509e39ccfcd68",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "e000796912f91e394357",
+    "url": "/todo/static/css/main.d1b4112b.chunk.css"
+  },
+  {
+    "revision": "e27acace09f917cc9010",
+    "url": "/todo/static/js/2.a3805cbb.chunk.js"
+  },
+  {
+    "revision": "0bdc4f0d8b3d84ae6a73bb9902b479ee",
+    "url": "/todo/static/js/2.a3805cbb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e000796912f91e394357",
+    "url": "/todo/static/js/main.50fd09f3.chunk.js"
+  },
+  {
+    "revision": "b61f4130aa10b1e3b8fd",
+    "url": "/todo/static/js/runtime-main.b109c730.js"
+  }
+]);
